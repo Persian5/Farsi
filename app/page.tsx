fd5e03ee84172d067f492f8d8227e73e843e5b49
Persian5 <iranopedia5@gmail.com>
2025-04-16 12:41:43 -0700

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Hello, this is the Farsi Academy landing page.</h1>
+      <p>You can now add lessons, dashboards, and more!</p>
+    </main>
+  );
+}
